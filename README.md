7dea33ef561720a0d90ef919d30ddd1bb8682135
pr701 <pr701@protonmail.com>
2022-11-03 18:34:41 +0300

    Update to IDA7

---
+++ README.md
@@ -0,0 +1,6 @@
+# SEGA DREAMCAST RAM Loader
+
+RAM Loader for IDA 7.x
+
+Version for older IDA is available in [this repository](https://github.com/lab313ru/sdc_loade).
+
--- src/sdc_loader.cpp
+++ src/sdc_loader.cpp
@@ -1,15 +1,18 @@
 /*
-*      Interactive disassembler (IDA).
-*      Copyright (c) 1990-2000 by Ilfak Guilfanov, <ig@datarescue.com>
-*      ALL RIGHTS RESERVED.
-*
+*	Interactive disassembler (IDA).
+*	Copyright (c) 1990-2022 by Ilfak Guilfanov, <ig@datarescue.com>
+*	ALL RIGHTS RESERVED.
 */
 
-#define VERSION "1.0"
+#define VERSION "1.1"
 /*
-*      SEGA DREAMCAST RAM Loader
-*      Author: Dr. MefistO [Lab 313] <meffi@lab313.ru>
+*	SEGA DREAMCAST RAM Loader for IDA 7.x
+*	Author: Dr. MefistO [Lab 313] <meffi@lab313.ru>
+*	Contribution: pr701
 */
+
+#include <cstdint>
+#include <string>
 
 #include <ida.hpp>
 #include <idp.hpp>
@@ -18,51 +21,84 @@
 #include <kernwin.hpp>
 #include <name.hpp>
 
+#ifndef MAX_FILE_FORMAT_NAME
+#define MAX_FILE_FORMAT_NAME	64
+#endif
+
 idaman loader_t ida_module_data LDSC;
+
+ea_t rams[] = { 0x8C000000, 0x0C000000, 0 };
+
+#define countof(x)	(sizeof(x) / sizeof(x[0]))
+#define rams_count	countof(rams)
+
+struct ram_chooser_t : public chooser_t
+{
+protected:
+	static const int widths_[];
+	static const char* const header_[];
+
+public:
+	ram_chooser_t() : chooser_t(CH_MODAL | CH_NOIDB, 1, widths_, header_, "Select Loading Address")
+	{}
+
+	// function that returns number of lines in the list
+	size_t idaapi get_count() const override
+	{
+		return rams_count;
+	}
+
+	// function that generates the list line
+	void idaapi get_row(
+		qstrvec_t* cols,
+		int* icon_,
+		chooser_item_attrs_t* attrs,
+		size_t n) const override
+	{
+		qstrvec_t& _cols = *cols;
+		char buffer[32];
+		qsnprintf(buffer, 32, "0x%08.8x", rams[n]);
+		_cols[0] = buffer;
+	}
+
+	cbret_t idaapi enter(size_t n) override
+	{
+		return cbret_t();
+	}
+};
+const int ram_chooser_t::widths_[] =
+{
+  32, // RAM Address
+};
+const char* const ram_chooser_t::header_[] =
+{
+  "RAM Address", // 0
+};
 
 //--------------------------------------------------------------------------
 static void print_version()
 {
-	static const char format[] = "SEGA DREAMCAST RAM loader plugin v%s;\nAuthor: Dr. MefistO [Lab 313] <meffi@lab313.ru>.";
+	static const char format[] = "SEGA DREAMCAST RAM loader plugin v%s;\n"
+		"Author: Dr.MefistO[Lab 313] <meffi@lab313.ru>.\n"
+		"Contribution: pr701";
 	info(format, VERSION);
 	msg(format, VERSION);
 }
 
 //--------------------------------------------------------------------------
-int idaapi accept_file(linput_t *li, char fileformatname[MAX_FILE_FORMAT_NAME], int n)
+int idaapi accept_file(
+	qstring* fileformatname,
+	qstring* processor,
+	linput_t* li,
+	const char* filename)
 {
-	if (n != 0) return 0;
-	
     int size = qlsize(li);
-
     if (size != 16 * 1024 * 1024 && size != 32 * 1024 * 1024)
         return 0;
 
-	qstrncpy(fileformatname, "SEGA DREAMCAST RAM", MAX_FILE_FORMAT_NAME);
-
+	*fileformatname = "SEGA DREAMCAST RAM";
+	*processor = "SH4";
 	return 1;
-}
-
-ea_t rams[] = { 0x8C000000, 0x0C000000, 0 };
-
-ulong idaapi sizer(void *obj)
-{
-    ea_t *p = (ea_t *)obj;
-    int count = 0;
-    while (*p++) count++;
-    return count;
-}
-
-char * idaapi getline(void *obj, ulong n, char *buf)
-{
-    ea_t *p = (ea_t *)obj;
-    
-    if (n == 0)
-        qstrncpy(buf, "RAM Address", strlen("RAM Address") + 1);
-    else
-        qsnprintf(buf, 32, "0x%08.8x", p[n - 1]);
-
-    return buf;
 }
 
 static void add_segment(ea_t start, ea_t end, const char *name, const char *class_name, const char *cmnt)
@@ -70,51 +106,50 @@ static void add_segment(ea_t start, ea_t end, const char *name, const char *clas
     if (!add_segm(0, start, end, name, class_name)) loader_failure();
     segment_t *segm = getseg(start);
     set_segment_cmt(segm, cmnt, false);
-    doByte(start, 1);
+	create_byte(start, 1);
 }
 
 //--------------------------------------------------------------------------
-static char device[MAXSTR] = "SH7750";
-static ioport_t *ports = NULL;
-static size_t numports = 0;
+static qstring device = "SH7750";
+static ioports_t ports;
 static const char cfgname[] = "sh3.cfg";
 
 static void load_symbols(void)
 {
-    free_ioports(ports, numports);
-    ports = read_ioports(&numports, cfgname, device, sizeof(device), NULL);
+	ports.clear();
+	read_ioports(&ports, &device, cfgname);
 }
 
 static void apply_symbols(void)
 {
     std::string name;
-    for (size_t i = 0; i < numports; ++i)
+    for (size_t i = 0; i < ports.size(); ++i)
     {
-        name.assign(ports[i].name);
+        name.assign(ports[i].name.c_str());
         size_t tail_pos = name.length() - 2;
         std::string tail = name.substr(tail_pos);
 
         if (tail[0] == '_')
         {
             if (tail == "_L")
-                doDwrd(ports[i].address, 4);
+				create_dword(ports[i].address, 4);
             else if (tail == "_W")
-                doWord(ports[i].address, 2);
+				create_word(ports[i].address, 2);
             else if (tail == "_B")
-                doByte(ports[i].address, 1);
+				create_byte(ports[i].address, 1);
 
             name = name.substr(0, tail_pos);
         }
         else if (tail == "WB") // "_WB"
         {
-            doWord(ports[i].address, 2);
+			create_word(ports[i].address, 2);
             name = name.substr(0, tail_pos);
         }
         else
-            doDwrd(ports[i].address, 4);
+			create_dword(ports[i].address, 4);
 
         set_name(ports[i].address, name.c_str(), SN_NOWARN);
-        set_cmt(ports[i].address, ports[i].cmt, false);
+        set_cmt(ports[i].address, ports[i].cmt.c_str(), false);
     }
 }
 
@@ -122,7 +157,7 @@ static void apply_symbols(void)
 void idaapi load_file(linput_t *li, ushort neflags, const char *fileformatname)
 {
     if (ph.id != PLFM_SH) {
-		set_processor_type("SH4", SETPROC_ALL | SETPROC_FATAL); // Motorola 68000
+		set_processor_type("SH4", SETPROC_LOADER_NON_FATAL); // Motorola 68000
 	}
 
     add_segment(0xFF000000, 0xFF000048, "CCN", "DATA", NULL);
@@ -143,17 +178,18 @@ void idaapi load_file(linput_t *li, ushort neflags, const char *fileformatname)
     add_segment(0xFFF00000, 0xFFF0000C, "HUDI", "DATA", NULL);
 
 	unsigned int size = qlsize(li); // size of rom
-
 	qlseek(li, 0, SEEK_SET);
 
-    int choice = choose(rams, 30, sizer, getline, "Select Loading Address");
+	ram_chooser_t* ch = new ram_chooser_t();
+	ssize_t choice = ch->choose();
+	if (choice <= chooser_base_t::NO_SELECTION)
+	{
+		error("Loading was canceled!");
+		return;
+	}
 
-    if (!choice)
-        error("Loading was canceled!");
-
-    bool t = add_segm(0, rams[choice - 1], rams[choice - 1] + 0x02000000, "RAM", "DATA");
-
-    file2base(li, 0, rams[choice - 1], rams[choice - 1] + size, FILEREG_PATCHABLE); // load rom to database
+    bool t = add_segm(0, rams[choice], rams[choice] + 0x02000000, "RAM", "DATA");
+    file2base(li, 0, rams[choice], rams[choice] + size, FILEREG_PATCHABLE); // load rom to database
 
     load_symbols();
     apply_symbols();
@@ -171,26 +207,26 @@ void idaapi load_file(linput_t *li, ushort neflags, const char *fileformatname)
 		| AF_NULLSUB //      0x0200          // Rename empty functions as nullsub_...
 		//| AF_LVAR //         0x0400          // Create stack variables
 		//| AF_TRACE //        0x0800          // Trace stack pointer
-		| AF_ASCII //        0x1000          // Create ascii string if data xref exists
+		| AF_STRLIT //        0x1000          // Create string if data xref exists
 		//| AF_IMMOFF //       0x2000          // Convert 32bit instruction operand to offset
 		//AF_DREFOFF //      0x4000          // Create offset if data xref to seg32 exists
 		| AF_FINAL //       0x8000          // Final pass of analysis
 		;
 	inf.af2 = 0
-		| AF2_JUMPTBL  //    0x0001          // Locate and create jump tables
+		| AF_JUMPTBL  //    0x0001          // Locate and create jump tables
 		//| AF2_DODATA  //     0x0002          // Coagulate data segs at the final pass
 		//| AF2_HFLIRT  //     0x0004          // Automatically hide library functions
-		| AF2_STKARG  //     0x0008          // Propagate stack argument information
-		| AF2_REGARG  //     0x0010          // Propagate register argument information
-		//| AF2_CHKUNI  //     0x0020          // Check for unicode strings
-		//| AF2_SIGCMT  //     0x0040          // Append a signature name comment for recognized anonymous library functions
-		| AF2_SIGMLT  //     0x0080          // Allow recognition of several copies of the same function
-		| AF2_FTAIL  //      0x0100          // Create function tails
-		| AF2_DATOFF  //     0x0200          // Automatically convert data to offsets
-		//| AF2_ANORET  //     0x0400          // Perform 'no-return' analysis
-		//| AF2_VERSP  //      0x0800          // Perform full SP-analysis (ph.verify_sp)
-		//| AF2_DOCODE  //     0x1000          // Coagulate code segs at the final pass
-		| AF2_TRFUNC  //     0x2000          // Truncate functions upon code deletion
+		| AF_STKARG  //     0x0008          // Propagate stack argument information
+		| AF_REGARG  //     0x0010          // Propagate register argument information
+		//| AF_CHKUNI  //     0x0020          // Check for unicode strings
+		//| AF_SIGCMT  //     0x0040          // Append a signature name comment for recognized anonymous library functions
+		| AF_SIGMLT  //     0x0080          // Allow recognition of several copies of the same function
+		| AF_FTAIL  //      0x0100          // Create function tails
+		| AF_DATOFF  //     0x0200          // Automatically convert data to offsets
+		//| AF_ANORET  //     0x0400          // Perform 'no-return' analysis
+		//| AF_VERSP  //      0x0800          // Perform full SP-analysis (ph.verify_sp)
+		//| AF_DOCODE  //     0x1000          // Coagulate code segs at the final pass
+		| AF_TRFUNC  //     0x2000          // Truncate functions upon code deletion
 		//| AF2_PURDAT  //     0x4000          // Control flow to data segment is ignored
 		//| AF2_MEMFUNC //    0x8000          // Try to guess member function types
 		;
